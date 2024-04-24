d242916e9d81040119c039265798263add4cfd37
Quang Minh <quangnm2209@gmail.com>
2024-04-24 16:09:11 +0700

    adding player movement and rotation

--- src/index.js
+++ src/index.js
@@ -8,12 +8,18 @@ const wss = new WebSocket.Server({ server });
 
 let clientId = 0;
 let clients = [];
+
 // WebSocket server
 wss.on("connection", (ws) => {
-
-    // start join server
+  // start join server
   clientId++;
-  clients.push({ id: clientId, client: ws, position: [0, 0, 0], name: "helo" });
+  clients.push({
+    id: clientId,
+    client: ws,
+    position: [0, 0, 0],
+    name: "helo",
+    rotation: [0, 0, 0],
+  });
   let id = clients.find((c) => c.client == ws).id;
 
   let datas = clients.map((item) => {
@@ -21,6 +27,7 @@ wss.on("connection", (ws) => {
       id: item.id,
       name: item.name,
       position: item.position,
+      rotation: item.rotation,
     };
   });
 
@@ -38,6 +45,7 @@ wss.on("connection", (ws) => {
         id: clientId,
         name: "new",
         position: [0, 0, 0],
+        rotation: [0, 0, 0],
       },
     ],
   };
@@ -46,8 +54,7 @@ wss.on("connection", (ws) => {
     if (client.readyState === WebSocket.OPEN) {
       if (client == ws) {
         client.send(JSON.stringify(finalData));
-      }
-      else{
+      } else {
         client.send(JSON.stringify(newData));
       }
     }
@@ -56,9 +63,14 @@ wss.on("connection", (ws) => {
   // end join server
 
   ws.on("message", (message) => {
-    
+    let data = message.toString();
+    try {
+      let jsonData = JSON.parse(data);
+      messageType[jsonData.eventName](wss, jsonData);
+    } catch (error) {
+      console.log(error);
+    }
   });
-
 
   //start leave server
   ws.on("close", () => {
@@ -66,11 +78,13 @@ wss.on("connection", (ws) => {
     if (currentClient != null) {
       wss.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify({
-            eventName:"Leave",
-            clientId: currentClient.id,
-            data:[]
-          }));
+          client.send(
+            JSON.stringify({
+              eventName: "Leave",
+              clientId: currentClient.id,
+              data: [],
+            })
+          );
         }
       });
     }
@@ -81,3 +95,28 @@ wss.on("connection", (ws) => {
 server.listen(3000, () => {
   console.log("Express server listening on port 3000");
 });
+
+// handle with message
+const handleMovement = (wss, data) => {
+  let tempData = {
+    eventName: "Movement",
+    clientId: data.clientId,
+    data: [
+      {
+        id: data.clientId,
+        name: "",
+        position: data.position,
+        rotation: data.rotation,
+      },
+    ],
+  };
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN){
+      client.send(JSON.stringify(tempData));
+    }
+  });
+};
+
+let messageType = {
+  Movement: handleMovement,
+};

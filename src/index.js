3b2c784f1d79933200166b8957e6821d28fb56b2
Quang Minh <quangnm2209@gmail.com>
2024-04-27 20:43:50 +0700

    adding select character through the internet

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
-const dotenv = require('dotenv');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin:"*"
-}));
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 dotenv.config();
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-const {handleMovement, handleAnimator} = require('./playerAction');
+const { handleMovement, handleAnimator } = require("./playerAction");
 
-const router = require('./router/index');
+const router = require("./router/index");
 
 router(app);
 
@@ -29,53 +31,6 @@ let clients = [];
 // WebSocket server
 wss.on("connection", (ws) => {
   // start join server
-  clientId++;
-  clients.push({
-    id: clientId,
-    client: ws,
-    position: [0, 0, 0],
-    name: "helo",
-    rotation: [0, 0, 0],
-  });
-  let id = clients.find((c) => c.client == ws).id;
-
-  let datas = clients.map((item) => {
-    return {
-      id: item.id,
-      name: item.name,
-      position: item.position,
-      rotation: item.rotation,
-    };
-  });
-
-  let finalData = {
-    eventName: "Player-JoinRoom",
-    clientId: id,
-    data: datas,
-  };
-
-  let newData = {
-    eventName: "Player-AddPlayerRoom",
-    clientId: id,
-    data: [
-      {
-        id: clientId,
-        name: "new",
-        position: [0, 0, 0],
-        rotation: [0, 0, 0],
-      },
-    ],
-  };
-
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      if (client == ws) {
-        client.send(JSON.stringify(finalData));
-      } else {
-        client.send(JSON.stringify(newData));
-      }
-    }
-  });
 
   // end join server
 
@@ -83,7 +38,60 @@ wss.on("connection", (ws) => {
     let data = message.toString();
     try {
       let jsonData = JSON.parse(data);
-      messageType[jsonData.eventName](wss, jsonData);
+      if (jsonData.eventName == "JoinServer") {
+        clientId++;
+        clients.push({
+          id: clientId,
+          client: ws,
+          position: jsonData.position,
+          characterName: jsonData.characterName,
+          name: jsonData.name,
+          rotation: jsonData.rotation,
+        });
+        let id = clients.find((c) => c.client == ws).id;
+
+        let datas = clients.map((item) => {
+          return {
+            id: item.id,
+            name: item.name,
+            position: item.position,
+            rotation: item.rotation,
+            characterName: item.characterName,
+          };
+        });
+
+        let finalData = {
+          eventName: "Player-JoinRoom",
+          clientId: id,
+          data: datas,
+        };
+
+        let newData = {
+          eventName: "Player-AddPlayerRoom",
+          clientId: id,
+          data: [
+            {
+              id: clientId,
+              position: jsonData.position,
+              characterName: jsonData.characterName,
+              name: jsonData.name,
+              rotation: jsonData.rotation,
+            },
+          ],
+        };
+
+        wss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            if (client == ws) {
+              client.send(JSON.stringify(finalData));
+            } else {
+              client.send(JSON.stringify(newData));
+            }
+          }
+        });
+      } else {
+        messageType[jsonData.eventName](wss, jsonData);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -117,13 +125,11 @@ mongoose
     console.log(`Your error ${err}`);
   });
 
-
 server.listen(3000, () => {
   console.log("Express server listening on port 3000");
 });
 
-
 let messageType = {
   "Player-Movement": handleMovement,
-  "Player-PlayerAnimator":handleAnimator
+  "Player-PlayerAnimator": handleAnimator,
 };
